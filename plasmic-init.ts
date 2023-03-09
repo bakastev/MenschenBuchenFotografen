ed0c5577791089b3b0e66cdfc5694d6955378f0f
bakastev <127332568+bakastev@users.noreply.github.com>
2023-03-09 12:14:20 +0100

    Add files via upload

---
+++ plasmic-init.ts
@@ -0,0 +1,14 @@
+import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+export const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: "iddbBH83KhUWFRXCoL8Dy2",  // ID of a project you are using
+      token: 
+"GTKyBOoNnQUKhBGJe33WFHXxWswC4hZ2RNESygf44JQ4Y5gXoHknXwyavv4xbFIGYoNNiuin8DkiDWQ"  
+// API token for that project
+    }
+  ],
+  // Fetches the latest revisions, whether or not they were unpublished!
+  // Disable for production to ensure you render only published changes.
+  preview: true,
+})
